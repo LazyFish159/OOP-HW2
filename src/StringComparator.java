bf08f560bedae59751e46f5a3ff02fad25e688ae
fcu-D0449419 <lazyfish159@gmail.com>
2016-10-14 11:48:32 +0800

    Update StringComparator.java

--- src/StringComparator.java
+++ src/StringComparator.java
@@ -4,19 +4,24 @@ public class StringComparator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int i=0;
+		while (i==0) {
+			
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter two strings");
 		String str1 = scanner.next();
 		String str2 = scanner.next();
-		System.out.printf("%s %s\n",str1,str2);
 		
 		if(str1.equalsIgnoreCase(str2)){
 			System.out.println("The two strings are the same.");
+			i+=1;
 		}
 		else{
 			System.out.println("The two strings are not the same.");
+			
+			
 		}
 			
 	}
-
+	}
 }
